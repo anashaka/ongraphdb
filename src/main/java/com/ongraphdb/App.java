cba8f4274d8f7fa7c7dd26e4de66bce849cac509
anashaka <akanashevich@gmail.com>
2010-12-05 02:21:51 +0300

    Init project folders structure
    Change-Id: I813b60e7e91768aa8046f102173b589a3c3edd6d
    Signed-off-by: anashaka <akanashevich@gmail.com>

---
+++ src/main/java/com/ongraphdb/App.java
@@ -0,0 +1,13 @@
+package com.ongraphdb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
