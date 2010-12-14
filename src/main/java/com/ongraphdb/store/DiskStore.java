fc7108bc9586ba4bc0a6cdae1fca311fa055bc93
anashaka <akanashevich@gmail.com>
2010-12-15 02:58:25 +0300

    Covered by tests Added initial implementation of  persistance layer
    Change-Id: I5efdd261ad7511cdd9e084aaf3450c6554a3163d
    Signed-off-by: anashaka <akanashevich@gmail.com>

--- src/main/java/com/ongraphdb/store/DiskStore.java
+++ src/main/java/com/ongraphdb/store/DiskStore.java
@@ -3,44 +3,167 @@ package com.ongraphdb.store;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
+import java.nio.channels.FileLock;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
-public abstract class DiskStore {
-	private String dataFile;
-	private String holesFile;
+public class DiskStore {
+	private final static int SIZE_BYTES = 4;
+	private String dataFileName;
 	private long dataMappedMemorySize;
-	
-	public DiskStore(String dataFile, String holesFile, long memBufferSize) {
-		this.dataFile = dataFile;
-		this.holesFile = holesFile;
-		dataMappedMemorySize =memBufferSize;
+	private FileChannel dataChannel;
+	private FileLock dataLock;
+	private MappedByteBuffer mappedDataBuffer;
+	private int nextPosition;
+	private long initialFileSize;
+	private Thread shutdownHookThread;
+
+	public DiskStore(String dataFileName, int dataMappedMemorySize,
+			int initialFileSize) {
+		this.dataFileName = dataFileName;
+		this.dataMappedMemorySize = dataMappedMemorySize;
+		this.initialFileSize = initialFileSize;
+	}
+
+	public void start() throws IOException {
+		File file = new File(dataFileName);
+		boolean newStore = file.exists() ? false : true;
+
+		RandomAccessFile dataFile = new RandomAccessFile(dataFileName, "rw");
+		dataFile.setLength(initialFileSize);
+		dataChannel = dataFile.getChannel();
+		dataLock = dataChannel.lock();
+		mappedDataBuffer = dataChannel.map(MapMode.READ_WRITE, 0,
+				dataMappedMemorySize);
+
+		if (newStore) {
+			mappedDataBuffer.putInt(SIZE_BYTES);
+			nextPosition = SIZE_BYTES + 1;
+		} else {
+			nextPosition = mappedDataBuffer.getInt();
+		}
+		shutdownHookThread = new Thread() {
+			public void run() {
+				try {
+					dataLock.release();
+					dataChannel.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};	
+		Runtime.getRuntime().addShutdownHook(shutdownHookThread);
 		
+
 	}
 	
-	public void init() throws IOException{
-		RandomAccessFile raf = new RandomAccessFile(dataFile, "rw" );
-		FileChannel channel = raf.getChannel();
-		channel.lock();
-		channel.map(MapMode.READ_WRITE, 0, dataMappedMemorySize);
-	}
-	
-	public byte[] readData(long pos, int blockSize){
-		return null;
+	public void shoutdown() throws IOException {
+		mappedDataBuffer.force();
+		mappedDataBuffer.clear();
+		dataLock.release();
+		dataChannel.close();
+		dataChannel = null;
+		unmap(mappedDataBuffer);
+		mappedDataBuffer = null;
+		System.gc();
+		Runtime.getRuntime().removeShutdownHook(shutdownHookThread);
 		
+
+	}	
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void unmap(final Object buffer) {
+		AccessController.doPrivileged(new PrivilegedAction() {
+			@SuppressWarnings("restriction")
+			public Object run() {
+				Method getCleanerMethod;
+				try {
+					getCleanerMethod = buffer.getClass().getMethod("cleaner",
+							new Class[0]);
+					getCleanerMethod.setAccessible(true);
+					sun.misc.Cleaner cleaner = (sun.misc.Cleaner) getCleanerMethod.invoke(buffer, new Object[0]);
+					cleaner.clean();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return null;
+
+			}
+		});
 	}
-	// maybe one method
-	//public byte[] rewriteData(long pos, byte[] data, int blockSize)
-	public byte[] writeData(long pos, byte[] data, int recordSize){
-		
+
+	public byte[] readData(int pos, int blockSize) throws IOException {
+		byte[] data;
+		if (pos + blockSize < dataMappedMemorySize) {
+			mappedDataBuffer.position(pos);
+			int dataSize = mappedDataBuffer.getInt();
+			if(pos + SIZE_BYTES + dataSize < dataMappedMemorySize){
+				data = new byte[dataSize];
+				mappedDataBuffer.get(data);
+			} else {	
+				ByteBuffer buffer = ByteBuffer.allocate(dataSize);
+				dataChannel.position(pos+SIZE_BYTES);
+				dataChannel.read(buffer);
+				data = buffer.array();
+				
+			}
+		} else {
+			ByteBuffer buffer = ByteBuffer.allocate(SIZE_BYTES);
+			dataChannel.position(pos);
+			dataChannel.read(buffer);
+			buffer.flip();
+			int dataSize = buffer.getInt();
+			buffer = ByteBuffer.allocate(dataSize);
+			dataChannel.read(buffer);
+			data = buffer.array();
+		}
 		return data;
-		
 	}
-	
-	public byte[] rewriteData(long pos, byte[] data, int blockSize){
-		
+
+	public int writeData(byte[] data, int blockSize) {
+		// check that pos in mapped buffer
+		int recordSize = getRecordSize(data.length, blockSize);
+		int pos = nextPosition;
+		nextPosition = nextPosition + recordSize;
+		mappedDataBuffer.position(0);
+		mappedDataBuffer.putInt(nextPosition);
+		try {
+			// check that pos in mapped buffer
+			if (pos + recordSize < mappedDataBuffer.limit()) {
+				mappedDataBuffer.position(pos);
+				mappedDataBuffer.putInt(data.length);
+				mappedDataBuffer.put(data);
+			} else {
+				ByteBuffer buffer = ByteBuffer.allocate(data.length
+						+ SIZE_BYTES);
+				buffer.putInt(data.length);
+				buffer.put(data);
+				buffer.flip();
+				dataChannel.write(buffer, pos);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			/*if (!nextPosition.compareAndSet(pos + recordSize, pos)) {
+				// writehole();
+			}*/
+		}
+		return pos;
+	}
+
+	public byte[] rewriteData(long pos, byte[] data, int blockSize) {
 		return data;
-		
 	}
+
+	protected int getRecordSize(int dataSize, int blockSize) {
+		return ((SIZE_BYTES + dataSize + blockSize - 1) / blockSize)
+				* blockSize;
+	}
+
 	
+
 }
---
+++ src/test/java/com/ongraphdb/store/DiskStoreTest.java
@@ -0,0 +1,88 @@
+package com.ongraphdb.store;
+
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DiskStoreTest {
+	private DiskStore store;
+	private static String DATA_FILE_NAME = "testData.dat";
+	private static int BLOCK_SIZE = 14;
+	private static byte[] DATA_10 = "1234567890".getBytes();
+	private static byte[] DATA_20 = "RECORD_20 RECORD_20 ".getBytes();
+	private static byte[] DATA_100 = "RECORD_100RECORD_100RECORD_100RECORD_100RECORD_100RECORD_100RECORD_100RECORD_100RECORD_100RECORD_100".getBytes();
+	private static int DATA_FILE_SIZE = 32*1024*1024;
+	private static int DATA_MAPPED_BUFFER_SIZE = 100;
+
+	@Before
+	public void initStore() throws IOException{
+		store = new DiskStore(DATA_FILE_NAME, DATA_MAPPED_BUFFER_SIZE, DATA_FILE_SIZE);
+		store.start();
+	}
+	
+	@After
+	public void shoutdowndAndCleanStore() throws IOException, InterruptedException{
+		store.shoutdown();
+		File f = new File(DATA_FILE_NAME);
+		boolean success = f.delete();
+	    assertTrue(success);
+	}
+	
+	@Test
+	public void createStore() throws IOException {		
+		File f = new File(DATA_FILE_NAME);
+		assertTrue(f.isFile());
+		assertTrue(f.exists());
+		assertEquals(f.length(), DATA_FILE_SIZE);
+	}
+	
+	@Test 
+	public void writeReadOneByOne() throws IOException {
+		int[] pos = new int[5];
+		pos[0] = store.writeData(DATA_10, BLOCK_SIZE);
+		assertEquals(5, pos[0]);		
+		assertEquals(new String(DATA_10), new String(store.readData(pos[0], BLOCK_SIZE)));
+		pos[1] = store.writeData(DATA_20, BLOCK_SIZE);
+		assertEquals(new String(DATA_20), new String(store.readData(pos[1], BLOCK_SIZE)));		
+		pos[2] = store.writeData(DATA_100, BLOCK_SIZE);
+		assertEquals(new String(DATA_100), new String(store.readData(pos[2], BLOCK_SIZE)));
+		pos[3] = store.writeData(DATA_20, BLOCK_SIZE);
+		pos[4] = store.writeData(DATA_100, BLOCK_SIZE);
+		assertEquals(new String(DATA_100), new String(store.readData(pos[4], BLOCK_SIZE)));
+	}
+	
+	@Test
+	public void reopenStore() throws IOException {
+		int[] pos = new int[6];
+		pos[0] = store.writeData(DATA_10, BLOCK_SIZE);
+		
+		pos[1] = store.writeData(DATA_20, BLOCK_SIZE);
+		pos[2] = store.writeData(DATA_100, BLOCK_SIZE);
+		store.shoutdown();
+		store.start();
+		pos[3] = store.writeData(DATA_10, BLOCK_SIZE);
+		pos[4] = store.writeData(DATA_20, BLOCK_SIZE);
+		pos[5] = store.writeData(DATA_100, BLOCK_SIZE);
+		assertEquals(new String(DATA_10), new String(store.readData(pos[0], BLOCK_SIZE)));
+		assertEquals(new String(DATA_20), new String(store.readData(pos[1], BLOCK_SIZE)));
+		assertEquals(new String(DATA_100), new String(store.readData(pos[2], BLOCK_SIZE)));
+		assertEquals(new String(DATA_10), new String(store.readData(pos[3], BLOCK_SIZE)));
+		assertEquals(new String(DATA_20), new String(store.readData(pos[4], BLOCK_SIZE)));
+		assertEquals(new String(DATA_100), new String(store.readData(pos[5], BLOCK_SIZE)));
+		
+	}
+	
+	@Test
+	public void exitStore() throws IOException {
+		Runtime.getRuntime().exit(0);	
+	}
+	
+	
+	
+} 
